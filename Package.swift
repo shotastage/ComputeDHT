a6ceec5a0c0189e5629839d6dbc8bdcd1e8c1fcb
shotastage <shota.shimazu@magicalsoft.app>
2026-03-31 22:18:56 +0900

    Add Package.resolved and update dependencies in Package.swift

--- Package.swift
+++ Package.swift
@@ -12,6 +12,9 @@ let package = Package(
             targets: ["ComputeDHT"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/swiftwasm/WasmKit.git", .upToNextMinor(from: "0.2.1")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
