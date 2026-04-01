8766a67e9901d40efdf3b6ccfe8b782032f518b5
shotastage <shota.shimazu@magicalsoft.app>
2026-04-01 11:19:05 +0900

    Refactor COperatingSystem.swift to use canImport for platform-specific imports

--- Sources/CBreeze/COperatingSystem.swift
+++ Sources/CBreeze/COperatingSystem.swift
@@ -5,10 +5,18 @@
 //  Created by Shota Shimazu on 2023/09/01.
 //
 
-#if os(Linux)
-    // Import Glibc on Linux
-    @_exported import Glibc
-#elseif os(OSX)
-    // Import Darwin on macOS
+#if canImport(Darwin.C)
+    // Darwin platforms (macOS, iOS, watchOS, tvOS)
     @_exported import Darwin.C
+#elseif canImport(Glibc)
+    // Linux and other platforms using glibc
+    @_exported import Glibc
+#elseif canImport(ucrt)
+    // Windows with Universal C Runtime (UCRT)
+    @_exported import ucrt
+#elseif canImport(WinSDK)
+    // Windows with WinSDK
+    @_exported import WinSDK
+#else
+    #error("Unsupported platform: no known C runtime module")
 #endif
